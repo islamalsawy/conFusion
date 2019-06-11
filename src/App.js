6b50a1cec9d7c0a07e1b57a67a8e48e49b420c08
IsLam Alsawy <esl14@yahoo.com>
2019-06-12 01:41:57 +0200

    Component part 2 -left and pass state

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from './components/MenuComponent'
 import "./App.css";
+import { dishes } from './shared/dishes'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: dishes
+        };
+    }
+
     render() {
         return ( <
             div className = "App" >
@@ -16,8 +24,8 @@ class App extends Component {
             NavbarBrand href = "/" > Ristornate Con Fusion < /NavbarBrand>{" "} <
             /div> <
             /Navbar> <
-            Menu / >
-            <
+            Menu dishes = { this.state.dishes }
+            /> <
             /div>
         );
     }
